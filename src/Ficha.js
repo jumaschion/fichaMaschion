43c266494971f8168ef4be8e157709f8f8c667d7
jumaschion <julia.maschion@hotmail.com>
2022-05-30 13:23:23 -0300

    netlify cli

--- src/Ficha.js
+++ src/Ficha.js
@@ -2,18 +2,18 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+// import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
+// import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import CheckroomOutlined from "@mui/icons-material/CheckroomOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
+// import TextareaAutosize from "@mui/material/TextareaAutosize";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Acrescimo from "./Acrescimo";
+// import Acrescimo from "./Acrescimo";
 
 function Copyright(props) {
   return (
